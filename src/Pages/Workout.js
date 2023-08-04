a279d9d4b39f7fc93f8e365154890855e720efc2
carsonyarborough <carson.yarborough@gmail.com>
2023-08-03 21:10:12 -0500

    button/form

--- src/Pages/Workout.js
+++ src/Pages/Workout.js
@@ -10,28 +10,43 @@ function Workout() {
     const exercises = selectedWorkout.exercises.map(
         (exercise) => <Fragment>
             <h3 key={exercise}>{exercise} </h3>
+
+            <form class="form-horizontal" action="/action_page.php">
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="Reps">Reps:</label>
+                    <div class="col-sm-10">
+                        <input type="Reps" class="form-control" id="Reps" placeholder="Enter Reps"></input>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="Weight">Weight:</label>
+                    <div class="col-sm-10">
+                        <input type="password" class="form-control" id="Weight" placeholder="Enter Weight"></input>
+                    </div>
+                </div>
+                <div class="form-group"></div>
+                <button type="button" class="btn btn-outline-danger btn-lg btn-block">Next Set</button>
+             
+            </form>
+
+
+
             {/* <div class="input-group mb3">
                 <div class="input-group-prepend">
-               
-                    {/* <div class="input-group-text" type="checkbox" >
-                        <input type="checkbox" class="form-control"></input>
-                        
-                    </div> */}
-                    {/* <label>Reps:</label>
+                    
+                    <label>Reps:</label>
                     <input type="text" class="form-control"></input>
                     <button type="button" class="btn btn-primary">save</button>
                 </div>
-            </div> */}
+            </div>
             <div class="input-group mb3">
                 <div class="input-group-prepend">
-                    {/* <div class="input-group-text" type="checkbox" >
-                        <input type="checkbox" class="form-control"></input>
-                    </div> */}
+
                     <label>Weight:</label>
                     <input type="text" class="form-control"></input>
                     <button type="button" class="btn btn-info">save</button>
                 </div>
-            </div>
+            </div> */}
 
         </Fragment >
     )
